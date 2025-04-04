57c7f4a728329e1556d50b5c6fd62c3f1d2cb4e8
parizval <anmolgoyal@gmail.com>
2025-04-05 06:10:58 +0800

    pushing aave data fetch along with selection function

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,6 +1,12 @@
 <script>
 import { createWalletClient, custom } from "viem";
 import { baseSepolia, avalancheFuji } from "viem/chains";
+import { ethers } from "ethers";
+import { UiPoolDataProvider, ChainId } from "@aave/contract-helpers";
+import * as markets from "@bgd-labs/aave-address-book";
+import { formatUserSummary, formatReserves } from "@aave/math-utils";
+import dayjs from "dayjs";
+
 
 export default {
     data() {
@@ -14,6 +20,13 @@ export default {
                 },
             ],
             walletLabel: "Connect Wallet",
+            ConditionTypeValue : 0,
+            ConditionType:[
+                { label: "Total Collateral Base Value", value: 0 },
+                { label: "Total Debt Base Value", value: 1 },
+                { label: "Loan-To-Value Ratio", value: 2 },
+                { label: "Total Debt USD Value", value: 3 },
+            ],
             walletClient: {},
             userAddress: "",
         };
@@ -36,12 +49,81 @@ export default {
 
                 this.walletLabel = "Connected: " + address.slice(0, 5) + "..." + address.slice(-4);
 
+                this.computeAaveData();
+                
 
-                console.log(`userAddress: ${address}`);
             } catch (error) {
                 console.log(error);
             }
         },
+        async computeAaveData(){
+
+            const provider = new ethers.providers.JsonRpcProvider(
+                    "https://polygon-mainnet.g.alchemy.com/v2/AAk893PV9j8JfH3lVW-EHguNXMVkk6S-",
+                );
+
+                console.log("Provider Connected");
+
+                console.log(
+                    `Pool_DATA_Provider: ${markets.AaveV3Polygon.UI_POOL_DATA_PROVIDER}`,
+                );
+
+                const poolDataProviderContract = new UiPoolDataProvider({
+                    uiPoolDataProviderAddress:
+                        markets.AaveV3Polygon.UI_POOL_DATA_PROVIDER,
+                    provider,
+                    chainId: ChainId.polygon,
+                });
+
+
+                const currentAccount =
+                    "0xE451141fCE63EB38e85F08a991fC5878Ee6335b2";
+
+                const reserves =
+                    await poolDataProviderContract.getReservesHumanized({
+                        lendingPoolAddressProvider:
+                            markets.AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
+                    });
+
+                    const userReserves =
+                    await poolDataProviderContract.getUserReservesHumanized({
+                        lendingPoolAddressProvider:
+                            markets.AaveV3Polygon.POOL_ADDRESSES_PROVIDER,
+                        user: currentAccount,
+                    });
+
+                const reservesArray = reserves.reservesData;
+                const baseCurrencyData = reserves.baseCurrencyData;
+                const userReservesArray = userReserves.userReserves;
+
+                const currentTimestamp = dayjs().unix();
+
+                const formattedReserves = formatReserves({
+                    reserves: reservesArray,
+                    currentTimestamp,
+                    marketReferenceCurrencyDecimals:
+                        baseCurrencyData.marketReferenceCurrencyDecimals,
+                    marketReferencePriceInUsd:
+                        baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+                });
+
+                const userSummary = formatUserSummary({
+                    currentTimestamp,
+                    marketReferencePriceInUsd:
+                        baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+                    marketReferenceCurrencyDecimals:
+                        baseCurrencyData.marketReferenceCurrencyDecimals,
+                    userReserves: userReservesArray,
+                    formattedReserves,
+                    userEmodeCategoryId: userReserves.userEmodeCategoryId,
+                });
+
+                console.log(userSummary);
+
+
+        }
+
+
     },
 };
 </script>
@@ -61,6 +143,14 @@ export default {
                     @click="ConnectWallet"
                 />
             </div>
+
+            <h2 class="font-bold text-2xl text-(--ui-secondary)">
+                Select Loan Condition Type
+            </h2>
+            <USelect v-model="ConditionTypeValue" :items="ConditionType" class="w-48" />
+
+
         </div>
+        
     </div>
 </template>
