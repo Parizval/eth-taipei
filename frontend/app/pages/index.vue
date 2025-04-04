355d92653e3ab81517e132fd1400e77271577581
parizval <anmolgoyal@gmail.com>
2025-04-05 05:37:58 +0800

    updated wallet connect button

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,4 +1,7 @@
 <script>
+import { createWalletClient, custom } from "viem";
+import { baseSepolia, avalancheFuji } from "viem/chains";
+
 export default {
     data() {
         return {
@@ -10,11 +13,34 @@ export default {
                     target: "_blank",
                 },
             ],
+            walletLabel: "Connect Wallet",
+            walletClient: {},
+            userAddress: "",
         };
     },
     methods: {
         async ConnectWallet() {
-            console.log("Wallet Click");
+            try {
+                console.log("Wallet Click");
+                this.walletClient = createWalletClient({
+                    chain: baseSepolia,
+                    transport: custom(window.ethereum),
+                });
+
+                this.walletClient.addChain({chain : avalancheFuji});
+    
+                await this.walletClient.switchChain({ id: baseSepolia.id });
+
+                const [address] = await this.walletClient.getAddresses();
+                this.userAddress = address;
+
+                this.walletLabel = "Connected: " + address.slice(0, 5) + "..." + address.slice(-4);
+
+
+                console.log(`userAddress: ${address}`);
+            } catch (error) {
+                console.log(error);
+            }
         },
     },
 };
@@ -24,25 +50,15 @@ export default {
     <div>
         <UNavigationMenu :items="navbar" class="w-full justify-center" />
         <div class="flex flex-col items-center justify-center gap-4 h-screen">
-            <h1 class="font-bold text-2xl text-(--ui-primary)">
-                Nuxt UI - Starter
-            </h1>
+            <h2 class="font-bold text-2xl text-(--ui-primary)">
+                Create Limit Order for your Aave Borrow Position
+            </h2>
 
             <div class="flex items-center gap-2">
                 <UButton
-                    label="Documentation"
+                    :label="walletLabel"
                     icon="i-lucide-square-play"
-                    to="https://ui.nuxt.com/getting-started/installation/nuxt"
-                    target="_blank"
-                />
-
-                <UButton
-                    label="GitHub"
-                    color="neutral"
-                    variant="outline"
-                    icon="i-simple-icons-github"
-                    to="https://github.com/nuxt/ui"
-                    target="_blank"
+                    @click="ConnectWallet"
                 />
             </div>
         </div>
