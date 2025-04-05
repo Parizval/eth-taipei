6d02732f5b128b3972310e34f29455cb573e8929
parizval <anmolgoyal@gmail.com>
2025-04-05 15:35:49 +0800

    updated deployment script for factort contract

--- smart-contracts/script/DeployFactory.s.sol
+++ smart-contracts/script/DeployFactory.s.sol
@@ -6,32 +6,40 @@ import {VaultFactory} from "../src/VaultFactory.sol";
 import {FactoryHelperConfig} from "./FactoryHelperConfig.s.sol";
 
 contract DeployFactory is Script {
+    VaultFactory public vaultFactory;
+
     function setUp() public {}
 
     function run() public {
         // Set up the environment
 
-        deployFactoryContract();
-    }
-
-    function deployFactoryContract() public returns (VaultFactory, FactoryHelperConfig) {
-        FactoryHelperConfig helperconfig = new FactoryHelperConfig();
-        FactoryHelperConfig.NetworkConfig memory config = helperconfig.getConfig();
-
+        // deployFactoryContract();
         vm.startBroadcast();
-        VaultFactory vault = new VaultFactory(
-            config.aavePoolAddress, // _aavePool
-            config.hyperlaneMailboxAddress, // _mailbox
-            config.usdcAddress, // _usdc
-            config.tokenMessenger, // _tokenMessenger
-            config.wormholeRelayer, // _wormholeRelayer
-            config.tokenBridge, // _wormholeTokenBridge
-            config.wormhole, // _wormholeCore
-            config.cctpChainId, // _cctpChainId
-            config.cctpValue // _cctpChainValue
-        );
+
+        vaultFactory = new VaultFactory();
 
         vm.stopBroadcast();
-        return (vault, helperconfig);
+
     }
+
+    // function deployFactoryContract() public returns (VaultFactory, FactoryHelperConfig) {
+    //     FactoryHelperConfig helperconfig = new FactoryHelperConfig();
+    //     FactoryHelperConfig.NetworkConfig memory config = helperconfig.getConfig();
+
+    //     vm.startBroadcast();
+    //     VaultFactory vault = new VaultFactory(
+    //         config.aavePoolAddress, // _aavePool
+    //         config.hyperlaneMailboxAddress, // _mailbox
+    //         config.usdcAddress, // _usdc
+    //         config.tokenMessenger, // _tokenMessenger
+    //         config.wormholeRelayer, // _wormholeRelayer
+    //         config.tokenBridge, // _wormholeTokenBridge
+    //         config.wormhole, // _wormholeCore
+    //         config.cctpChainId, // _cctpChainId
+    //         config.cctpValue // _cctpChainValue
+    //     );
+
+    //     vm.stopBroadcast();
+    //     return (vault, helperconfig);
+    // }
 }
