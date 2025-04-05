74df6fbc34ec251fc4431a0fb24dc1b60e255d54
parizval <anmolgoyal@gmail.com>
2025-04-05 15:35:29 +0800

    removed code to reduce deployment cost

--- smart-contracts/src/VaultFactory.sol
+++ smart-contracts/src/VaultFactory.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Vault} from "./Vault.sol";
-
 contract VaultFactory {
-    event VaultCreated(address indexed vaultAddress, address indexed owner);
+
+    event VaultUpdated(address indexed vaultAddress, address indexed owner);
     event CrossChainTransfer(address indexed vaultAddress, address indexed token, uint256 amount);
 
     error InvalidVault();
@@ -12,42 +11,9 @@ contract VaultFactory {
 
     address public immutable owner;
 
-    address public immutable aavePool;
-
-    address public immutable mailbox;
-
-    address public immutable usdc;
-    address public immutable tokenMessenger;
-
-    address public immutable wormholeRelayer;
-    address public immutable wormholeTokenBridge;
-    address public immutable wormholeCore;
-
-    uint32 public immutable destinationCCTPChainId;
-    uint32 public immutable destinationCCTPChainValue;
-
-    constructor(
-        address _aavePool,
-        address _mailbox,
-        address _usdc,
-        address _tokenMessenger,
-        address _wormholeRelayer,
-        address _wormholeTokenBridge,
-        address _wormholeCore,
-        uint32 _destinationCCTPChainId,
-        uint32 _destinationCCTPChainValue
+    constructor(        
     ) {
         owner = msg.sender;
-
-        aavePool = _aavePool;
-        mailbox = _mailbox;
-        usdc = _usdc;
-        tokenMessenger = _tokenMessenger;
-        wormholeRelayer = _wormholeRelayer;
-        wormholeTokenBridge = _wormholeTokenBridge;
-        wormholeCore = _wormholeCore;
-        destinationCCTPChainId = _destinationCCTPChainId;
-        destinationCCTPChainValue = _destinationCCTPChainValue;
     }
 
     modifier onlyOwner() {
@@ -59,29 +25,12 @@ contract VaultFactory {
 
     mapping(uint32 => uint16) public worldChainId;
 
-    function createVault() external returns (address) {
-        if (vaults[msg.sender] != address(0)) {
-            revert VaultAlreadyExists(vaults[msg.sender]);
-        }
+    function addVault(address _vault) external {
+        
+        vaults[msg.sender] = _vault;
 
-        Vault vault = new Vault(
-            msg.sender,
-            aavePool,
-            mailbox,
-            usdc,
-            tokenMessenger,
-            wormholeRelayer,
-            wormholeTokenBridge,
-            wormholeCore,
-            destinationCCTPChainId,
-            destinationCCTPChainValue
-        );
+        emit VaultUpdated(msg.sender, _vault);
 
-        vaults[msg.sender] = address(vault);
-
-        emit VaultCreated(address(vault), msg.sender);
-
-        return address(vault);
     }
 
     function emitCrossChainTransfer(address _owner, address _token, uint256 _amount) external {
