19ad81c7ec0fec1dcbf26ddd09f0a11040c71b0f
parizval <anmolgoyal@gmail.com>
2025-04-05 13:02:51 +0800

    added deployment file for factory contracts

---
+++ smart-contracts/script/DeployFactory.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {VaultFactory} from "../src/VaultFactory.sol";
+import {FactoryHelperConfig} from "./FactoryHelperConfig.s.sol";
+
+contract deployVault is Script {
+    function setUp() public {}
+
+    function run() public {
+        // Set up the environment
+    }
+
+    function deployFactoryContract() public returns (VaultFactory, FactoryHelperConfig) {
+        FactoryHelperConfig helperconfig = new FactoryHelperConfig();
+        FactoryHelperConfig.NetworkConfig memory config = helperconfig.getConfig();
+
+        vm.startBroadcast();
+        VaultFactory vault = new VaultFactory(
+            config.aavePoolAddress, // _aavePool
+            config.hyperlaneMailboxAddress, // _mailbox
+            config.usdcAddress, // _usdc
+            config.tokenMessenger, // _tokenMessenger
+            config.wormholeRelayer, // _wormholeRelayer
+            config.tokenBridge, // _wormholeTokenBridge
+            config.wormhole, // _wormholeCore
+            config.cctpChainId, // _cctpChainId
+            config.cctpValue // _cctpChainValue
+        );
+
+        vm.stopBroadcast();
+        return (vault, helperconfig);
+    }
+}
---
+++ smart-contracts/script/FactoryHelperConfig.s.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+
+abstract contract ChainIdConfiguration {
+    uint256 public constant BASE_SEPOLIA = 84532;
+
+    // uint256 public constant BASE_MAINNET = 84531;
+
+    uint256 public constant FUJI = 43113;
+}
+
+contract FactoryHelperConfig is Script, ChainIdConfiguration {
+    struct NetworkConfig {
+        address owner;
+        address aavePoolAddress;
+        address hyperlaneMailboxAddress;
+        address usdcAddress;
+        address tokenMessenger;
+        address wormholeRelayer;
+        address tokenBridge;
+        address wormhole;
+        uint32 cctpChainId;
+        uint32 cctpValue;
+    }
+
+    NetworkConfig public localNetworkConfig;
+    mapping(uint256 => NetworkConfig) public networkConfigs;
+
+    constructor() {
+        networkConfigs[BASE_SEPOLIA] = getBaseSepoliaConfig();
+        networkConfigs[FUJI] = getFujiConfig();
+    }
+
+    function getNetworkConfig() public view returns (NetworkConfig memory) {
+        if (block.chainid == BASE_SEPOLIA) {
+            return networkConfigs[BASE_SEPOLIA];
+        } else if (block.chainid == FUJI) {
+            return networkConfigs[FUJI];
+        } else {
+            revert("Unsupported chain");
+        }
+    }
+
+    function getConfig() public view returns (NetworkConfig memory) {
+        return networkConfigs[block.chainid];
+    }
+
+    function getBaseSepoliaConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            owner: 0xA01f6403d49857b58D3794C12E028c3681b24F98,
+            aavePoolAddress: 0x1234567890123456789012345678901234567890, // TODO
+            hyperlaneMailboxAddress: 0x6966b0E55883d49BFB24539356a2f8A673E02039,
+            usdcAddress: 0x1234567890123456789012345678901234567890,
+            tokenMessenger: 0x1234567890123456789012345678901234567890,
+            wormholeRelayer: 0x93BAD53DDfB6132b0aC8E37f6029163E63372cEE,
+            tokenBridge: 0x86F55A04690fd7815A3D802bD587e83eA888B239,
+            wormhole: 0x79A1027a6A159502049F10906D333EC57E95F083,
+            cctpChainId: 43113,
+            cctpValue: 1
+        });
+    }
+
+    function getFujiConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({
+            owner: 0xA01f6403d49857b58D3794C12E028c3681b24F98,
+            aavePoolAddress: 0x1234567890123456789012345678901234567890, // TODO
+            hyperlaneMailboxAddress: 0x5b6CFf85442B851A8e6eaBd2A4E4507B5135B3B0,
+            usdcAddress: 0x1234567890123456789012345678901234567890,
+            tokenMessenger: 0x1234567890123456789012345678901234567890,
+            wormholeRelayer: 0xA3cF45939bD6260bcFe3D66bc73d60f19e49a8BB,
+            tokenBridge: 0x61E44E506Ca5659E6c0bba9b678586fA2d729756,
+            wormhole: 0x7bbcE28e64B3F8b84d876Ab298393c38ad7aac4C,
+            cctpChainId: 84532,
+            cctpValue: 1
+        });
+    }
+}
